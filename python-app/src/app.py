3e47efade7a51f850358a0aa9a222c6541019a02
jieun-14 <j.l@kt.com>
2026-03-01 19:36:43 +0900

    추가추가~

---
+++ python-app/src/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import datetime
+import socket
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/details1')
+def details1():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/api/v1/details2')
+def details2():
+    return jsonify({
+        'message': 'Hello World!',
+        'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'hostname': socket.gethostname(),
+        'ip': request.remote_addr
+    })
+
+
+@app.route('/api/v1/healthz')
+def healthz():
+    return jsonify({
+        'status': 'up'
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
+# '/api/v1/details' http://127.0.0.1:5000/api/v1/details 접속!!!
+# '/api/v1/healthz'
+
