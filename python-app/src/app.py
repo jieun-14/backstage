079fffc1e7884d78a96c3da5b82a5ab8d64f0c25
jieun-14 <j.l@kt.com>
2026-03-28 21:18:27 +0900

    fix: update healthz status string to include Korean characters

--- python-app/src/app.py
+++ python-app/src/app.py
@@ -25,7 +25,7 @@ def details2():
 @app.route('/api/v1/healthz')
 def healthz():
     return jsonify({
-        'status': 'UPUPupup'
+        'status': 'UPUPupupㅁㅁㅁ'
     }), 200
 
 if __name__ == '__main__':
