44304fa4b38d559c567380e57d44a1017c21a660
jieun-14 <j.l@kt.com>
2026-03-28 22:32:52 +0900

    fix: correct typo in health check status string

--- python-app/src/app.py
+++ python-app/src/app.py
@@ -25,7 +25,7 @@ def details2():
 @app.route('/api/v1/healthz')
 def healthz():
     return jsonify({
-        'status': 'UPUPupupㅁㅁㅁ'
+        'status': 'UPUPupupㅁㅁaaㅁ'
     }), 200
 
 if __name__ == '__main__':
