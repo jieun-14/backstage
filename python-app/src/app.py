67740e10f4e235707a8c32b85962a645ef135142
jieun-14 <j.l@kt.com>
2026-03-28 21:14:19 +0900

    fix: update health check status string to UPUPupup

--- python-app/src/app.py
+++ python-app/src/app.py
@@ -25,7 +25,7 @@ def details2():
 @app.route('/api/v1/healthz')
 def healthz():
     return jsonify({
-        'status': 'UP'
+        'status': 'UPUPupup'
     }), 200
 
 if __name__ == '__main__':
